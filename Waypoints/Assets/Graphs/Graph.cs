e61c8960e627431b88601fb2b9f6631bd81ddbd9
Lucille Amanda Jureidini <145380670+cdblast@users.noreply.github.com>
2025-01-14 11:46:22 -0800

    A Simple Graph API Part 3 (inncomplete)
    at timestamp 5:38

--- Waypoints/Assets/Graphs/Graph.cs
+++ Waypoints/Assets/Graphs/Graph.cs
@@ -66,7 +66,7 @@ public class Graph
             Node thisNode = open[i];
             if (thisNode.getId() == endId)
             {
-                //reconstructPath(start,end);
+                ReconstructPath(start,end);
                 return true;
             }
 
@@ -106,6 +106,20 @@ public class Graph
             }
         }
         return false;
+    }
+
+    public void ReconstructPath(Node startId, Node endId)
+    {
+        pathList.Clear();
+        pathList.Add(endId);
+
+        var p = endId.cameFrom;
+        while(p != startId && p != null)
+        {
+            pathList.Insert(0, p);
+            p = p.cameFrom;
+        }
+        pathList.Insert(0, startId);
     }
 
     float distance(Node a, Node b)
